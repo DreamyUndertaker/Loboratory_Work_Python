d6d9f8a39c629d98b60f455a17fcd552ef897bc9
Vadim Zalogin <vadiimreg73@gmail.com>
2022-02-07 20:54:17 +0400

    осталась 5 лаба 2 задание, + выписанны задания.

--- лабы/5 лаба 1 задание/5 лаба 1 задание 6 вариант.py
+++ лабы/5 лаба 1 задание/5 лаба 1 задание 6 вариант.py
@@ -1,3 +1,9 @@
-n = int(input())
-lst = [[i for _ in range(n)] for i in range(n)]
-print(*lst, sep = '\n')
+from ast import comprehension
+
+
+n = int(input("введите число \n"))
+list = [[i for _ in range(n)] for i in range(n)]
+print(*list, sep = '\n')
+# Вводится натуральное число N. Необходимо сгенерировать вложеный список с помощью list comprehension,
+# размером N x N, где первая строка содержала бы все нули, вторая единицы, третья все двойки 
+# и так до N, результат вывести на экран.
--- лабы/лекция/5 лаба 1 задание 6 вариант (2).py
+++
@@ -1,7 +0,0 @@
-name = "Vadim"
-lastName = "Zalogin"
-group = "ISDO-21"
-
-#str = ("Меня зовут {0} Моя фамилия {1} Я учусь в группе {2}".format(name, lastName, group))
-
-print(f"My name {name} My surname {lastName} I am studing in {group}")
