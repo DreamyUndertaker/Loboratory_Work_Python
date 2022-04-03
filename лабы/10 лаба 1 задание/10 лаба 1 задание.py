7fa0048d941afe2b701f42410e41499183910c3f
Вадим Залогин <vadiimreg73@outlook.com>
2022-04-03 12:06:26 +0400

    предмет сдан

--- лабы/10 лаба 1 задание/10 лаба 1 задание.py
+++ лабы/10 лаба 1 задание/10 лаба 1 задание.py
@@ -3,10 +3,12 @@ from random import randint
 
 pi = 3.14
 
-for i in range(0, 5):
-    r = randint(10, 10000)
-    s = pi * r**2
-    print(round(s, 4))
+def function():
+    for i in range(0, 5):
+        r = randint(10, 10000)
+        s = pi * r**2
+        print(round(s, 4))
+function()
 
 # Напишите генератор, который бы возвращал площади кругов 
 # с радиусами в диапазоне от 10 до 10 000. Вывести на экран первые пять значений 
